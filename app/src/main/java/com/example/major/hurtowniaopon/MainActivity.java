0b1f260cccbb98123dc956fdcbaf99420e1d6e4f
MajorProjekt <oskarmajorczyk96@gmail.com>
2018-12-02 22:57:03 +0100

    0.0.2 + Dodano layout MainKlient
    0.0.1
    + Dodano layout MainActivity

--- app/src/main/java/com/example/major/hurtowniaopon/MainActivity.java
+++ app/src/main/java/com/example/major/hurtowniaopon/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.major.hurtowniaopon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button klientButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        klientButton = (Button) findViewById(R.id.menu_Klient);
+        klientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainKlientActivity();
+            }
+        });
+    }
+
+    private void openMainKlientActivity(){
+        Intent intent = new Intent(this, MainKlient.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/major/hurtowniaopon/MainKlient.java
+++ app/src/main/java/com/example/major/hurtowniaopon/MainKlient.java
@@ -1,13 +1,35 @@
 package com.example.major.hurtowniaopon;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainKlient extends AppCompatActivity {
+    private ImageButton addKlientButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_klient);
+
+        addKlientButton = (ImageButton) findViewById(R.id.AddButtonKlient);
+       addKlientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddKlientActivity();
+            }
+        });
+
     }
+
+    private void openAddKlientActivity(){
+        Intent intent = new Intent(this, AddKlient.class);
+        startActivity(intent);
+    }
+
 }
