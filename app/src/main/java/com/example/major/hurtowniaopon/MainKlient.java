9ba269ae4b04bbfcc4c1cf2c61385023a81bd737
MajorProjekt <oskarmajorczyk96@gmail.com>
2018-12-02 21:26:27 +0100

    0.0.2 + Dodano layout MainKlient
    0.0.1
    + Dodano layout MainActivity

---
+++ app/src/main/java/com/example/major/hurtowniaopon/MainKlient.java
@@ -0,0 +1,13 @@
+package com.example.major.hurtowniaopon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainKlient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_klient);
+    }
+}
