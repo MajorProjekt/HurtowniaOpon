eb9760ec158db50868d3121311f0f2b3046dc7d0
MajorProjekt <oskarmajorczyk96@gmail.com>
2018-12-18 23:22:59 +0100

    0.0.2 + Dodano layout MainKlient
    0.0.1
    + Dodano layout MainActivity

--- app/src/main/java/com/example/major/hurtowniaopon/MainActivity.java
+++ app/src/main/java/com/example/major/hurtowniaopon/MainActivity.java
@@ -8,20 +8,37 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private Button klientButton;
+
+    DatabaseHelper myDb;
+
+   protected void przyciskKlient(){
+
+
+
+       klientButton = (Button) findViewById(R.id.menu_Klient);
+       klientButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openMainKlientActivity();
+           }
+       });
+
+   }
+
+   protected void stworzBazeDanych(){
+       myDb = new DatabaseHelper(this);
+   }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        klientButton = (Button) findViewById(R.id.menu_Klient);
-        klientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainKlientActivity();
-            }
-        });
+        przyciskKlient();
+
+
     }
 
     private void openMainKlientActivity(){
